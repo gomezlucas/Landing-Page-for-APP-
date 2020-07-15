7a07c63d34b8cf06de51fb07cf995e807e1bb6c9
Lucas <gomez-lucas@live.com.ar>
2020-07-15 19:44:07 -0300

    require php mailer modified

--- correo.php
+++ correo.php
@@ -3,9 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/PHPMailer/src/Exception.php';
-require 'phpmailer/PHPMailer/src/PHPMailer.php';
-require 'phpmailer/PHPMailer/src/SMTP.php';
+require './phpmailer/PHPMailer/src/Exception.php';
+require './phpmailer/PHPMailer/src/PHPMailer.php';
+require './phpmailer/PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
