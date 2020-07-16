36fb51ab9ec77df1de752e0aa28ece550038b22b
Lucas <gomez-lucas@live.com.ar>
2020-07-16 01:56:36 -0300

    Swiper and lightbox working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 (function ($) {
     "use strict"; // Start of use strict
 
-      
+
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
         target: "#mainNav",
@@ -10,7 +10,7 @@
 
     // Collapse Navbar
     var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100 ) {
+        if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
         } else {
             $("#mainNav").removeClass("navbar-shrink");
@@ -21,43 +21,48 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
-   // Collapse the navbar link is clicked 
-   $('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+    // Collapse the navbar link is clicked 
+    $('.navbar-nav>li>a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
 
 
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    // Pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 20,
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 40,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 50,
-        },
-      }
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            },
+        }
+    })
 
- 
-})
+
+    /* Light Box */
+
+    lightbox.option({
+        'showImageNumberLabel': false
+    })
 
 
 })(jQuery); // End of use strict
